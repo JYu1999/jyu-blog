b75b5305e86d751c48f3f84a6d22d6405a315d2b
JYu1999 <jk29666338@gmail.com>
2025-03-02 22:39:32 +0800

    revert: 暫時放棄簡體轉繁體

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,31 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Configure Carbon's diffForHumans for Traditional Chinese
-        Carbon::setLocale(App::getLocale());
-        
-        // Override Traditional Chinese (zh) localization to ensure it uses traditional characters
-        if (App::getLocale() === 'zh') {
-            Carbon::setTranslations('zh', [
-                'year' => '年',
-                'years' => '年',
-                'month' => '個月',
-                'months' => '個月',
-                'week' => '週',
-                'weeks' => '週',
-                'day' => '天',
-                'days' => '天',
-                'hour' => '小時',
-                'hours' => '小時',
-                'minute' => '分鐘',
-                'minutes' => '分鐘',
-                'second' => '秒',
-                'seconds' => '秒',
-                'ago' => '前',
-                'from_now' => '後',
-                'after' => '後',
-                'before' => '前',
-            ]);
-        }
+        //
     }
 }
